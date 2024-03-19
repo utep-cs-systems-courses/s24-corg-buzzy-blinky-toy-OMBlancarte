fa0f55fa5dd6396a5030415a9791d4d779368237
OMBlancarte <oblancar66@gmail.com>
2024-03-19 16:44:56 -0600

    Extension of 3-led-blink

---
+++ demos/3-led-both-blink/led.h
@@ -0,0 +1,9 @@
+#ifndef led_included
+#define led_included
+
+#define LED_RED BIT6               // P1.0 Flipped to work properly
+#define LED_GREEN BIT0             // P1.6
+#define LEDS (BIT0 | BIT6)
+
+
+#endif // included
